3fc6bd557eef23c5ef0a5feaf5f0e7db29cef50f
Junhao Zhang <junhaodev@gmail.com>
2021-01-02 12:49:12 -0600

    Add some toys and made movement better

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public float turnSpeed;
     public Transform player;
     public Rigidbody rb;
 
     void FixedUpdate()
     {
         // rotation
-        float horizontal = Input.GetAxis("Horizontal");
+        float horizontal = Input.GetAxis("Horizontal") * turnSpeed;
 
         Quaternion rotation = Quaternion.AngleAxis(horizontal, Vector3.up);
         player.localRotation *= rotation;
@@ -21,10 +22,17 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity += player.TransformDirection(direction * speed * Time.fixedDeltaTime);
         rb.velocity.Normalize();
 
+
         // brakes
         if (Input.GetKey(KeyCode.X))
         {
             rb.velocity = new Vector3(0, 0, 0);
+        }
+
+        // reset
+        if (Input.GetKey(KeyCode.R))
+        {
+            player.localRotation = Quaternion.identity;
         }
     }
 }
