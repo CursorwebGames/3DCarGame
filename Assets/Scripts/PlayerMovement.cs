58a51059594fd4055356b5060168a36e94a74c4e
Junhao Zhang <junhaodev@gmail.com>
2021-01-02 12:20:29 -0600

    Fixed Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
+    public Transform player;
     public Rigidbody rb;
 
     void FixedUpdate()
     {
+        // rotation
         float horizontal = Input.GetAxis("Horizontal");
+
+        Quaternion rotation = Quaternion.AngleAxis(horizontal, Vector3.up);
+        player.localRotation *= rotation;
+
+
+        // movement
         float vertical = Input.GetAxis("Vertical");
-
-        Vector3 direction = new Vector3(horizontal, 0, vertical);
-        direction.Normalize();
-
-        rb.velocity += transform.TransformDirection(direction * speed * Time.fixedDeltaTime);
+        Vector3 direction = new Vector3(0, 0, vertical);
+        rb.velocity += player.TransformDirection(direction * speed * Time.fixedDeltaTime);
         rb.velocity.Normalize();
 
+        // brakes
         if (Input.GetKey(KeyCode.X))
         {
             rb.velocity = new Vector3(0, 0, 0);
